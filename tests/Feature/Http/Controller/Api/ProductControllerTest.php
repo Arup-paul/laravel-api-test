d27d27b5f1d664c8ecfa2df4ed4c1dec44cafd93
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2021-05-18 16:31:21 +0600

    product store test

--- tests/Feature/Http/Controller/Api/ProductControllerTest.php
+++ tests/Feature/Http/Controller/Api/ProductControllerTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature\Http\Controller\Api;
 
+use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class ProductControllerTest extends TestCase
@@ -13,10 +16,25 @@ class ProductControllerTest extends TestCase
      */
     public function can_create_a_product()
     {
+        $faker  = Factory::create();
         $response =  $this->json('POST','/api/products',[
-
+          'name'  => $name = $faker->company,
+          'slug' => Str::slug($name),
+          'price' => $price = random_int(10,100)
         ]);
 
-        $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'id','name','slug','price','created_at'
+        ])->assertJson([
+            'name' => $name,
+            'slug'=> Str::slug($name),
+            'price' => $price
+        ])->assertStatus(201);
+
+        $this->assertDatabaseHas('products',[
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'price' => $price
+        ]);
     }
 }
